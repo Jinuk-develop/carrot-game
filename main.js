e3e53de3200c77aa485e2cc62623195365ecc5c6
Jinuk-develop <127473357+Jinuk-develop@users.noreply.github.com>
2023-06-30 23:37:54 +0900

    JS - timer, popUp
    JS - timer, popUp

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const carrotSize = 80;
 const carrotCount = 5;
 const bugCount = 5;
+const GAME_DURATION_SEC = 65;
 
 const field = document.querySelector('.game__field');
 const fieldReat = field.getBoundingClientRect();
@@ -9,6 +10,7 @@ const fieldReat = field.getBoundingClientRect();
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
+const gamePopUp = document.querySelector('.pop-up');
 
 // 초기값
 let started = false;
@@ -30,19 +32,15 @@ function startGame() {
   inItGame();
   showStopBtn();
   showTimerAndScore();
+  startGameTimer();
 }
 
 function stopGame() {
-  showPlayBtn();
-  removeTimerAndScore();
+  stopGameTimer();
+  PopUpToggle();
 }
 
 // 버튼 play, stop 바꾸기
-function showPlayBtn() {
-  const icon = gameBtn.querySelector('.fa-stop');
-  icon.classList.remove('fa-stop');
-  icon.classList.add('fa-play');
-}
 
 function showStopBtn() {
   const icon = gameBtn.querySelector('.fa-play');
@@ -56,19 +54,54 @@ function showTimerAndScore() {
   gameScore.style.visibility = 'visible';
 }
 
-function removeTimerAndScore() {
-  gameTimer.style.visibility = 'hidden';
-  gameScore.style.visibility = 'hidden';
+// function removeTimerAndScore() {
+//   gameTimer.style.visibility = 'hidden';
+//   gameScore.style.visibility = 'hidden';
+// }
+
+// 타이머 플레이,
+function startGameTimer() {
+  let remainingTimeSec = GAME_DURATION_SEC;
+  updateTimerText(remainingTimeSec);
+  timer = setInterval(() => {
+    if (remainingTimeSec <= 0) {
+      clearInterval(timer);
+      return;
+    }
+    updateTimerText(--remainingTimeSec);
+  }, 1000);
+}
+function updateTimerText(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+// 일시정지, 팝업창 나오기.
+function PopUpToggle() {
+  gamePopUp.classList.toggle('pop-up--hide');
+  gameBtn.classList.toggle('game__button--hide');
 }
 
 // 버튼 누르면 해당 당근 삭제.
 field.addEventListener('click', (event) => {
-  ev;
+  const carrotId = event.target.dataset.id;
+  if (carrotId) {
+    const carrotDeleted = document.querySelector(
+      `.carrot[data-id='${carrotId}']`
+    );
+    carrotDeleted.remove();
+  }
 });
 
 // 아이템 생성.
 function inItGame() {
   field.innerHTML = '';
+  gameScore.innerText = carrotCount;
   //아이템 생성한 뒤 field에 추가
   addItem('carrot', carrotCount, 'img/carrot.png');
   addItem('bug', bugCount, 'img/bug.png');
